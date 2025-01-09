03848787bd7274e36afdecb8c9929eeff48d80e0
Madhav <madhavsingh.relish@gmail.com>
2025-01-09 21:15:21 +0530

    Added basic collaboration and color, stroke change feature

--- apps/frontend/app/room/page.tsx
+++ apps/frontend/app/room/page.tsx
@@ -1,12 +1,19 @@
+'use client'
 import Board from '@/components/Board';
 import Menu from '@/components/Menu'
 import Whiteboard from '@/components/Whiteboard';
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import {SketchField, Tools} from 'react-sketch';
 
 type Props = {}
 
 const page = (props: Props) => {
+  const [brushColor, setBrushColor] = useState('black');
+  const [brushSize, setBrushSize] = useState<number>(5);
+
+  useEffect(() => {
+    console.log("CanvasDrawing ", brushSize);
+  }, [brushSize]);
   return (
     <div >
         <div className='flex justify-center'>
@@ -15,9 +22,21 @@ const page = (props: Props) => {
         </div>
         <div>Welcome to Room: </div>
 
-        {/* <Whiteboard/> */}
-        <Board/>
+        <div className='tools' >
+          <div>
+            <span>Color: </span>
+            <input type="color" value={brushColor} onChange={(e) => setBrushColor(e.target.value)} />
+          </div>
+          <div>
+            <span>Size: </span>
+            <input type="range" color='#fac176'
+              min="1" max="100" value={brushSize} onChange={(e) => setBrushSize(Number(e.target.value))} />
+            <span>{brushSize}</span>
+          </div>
+        </div>
 
+        {/* <Whiteboard/> */}
+        <Board brushColor={brushColor} brushSize={brushSize} />
     </div>
   )
 }
--- apps/frontend/components/Board.tsx
+++ apps/frontend/components/Board.tsx
@@ -1,34 +1,66 @@
-'use client'
+import React, { useRef, useEffect, useState } from 'react';
+import io, { Socket } from 'socket.io-client';
 
-import React, { useEffect, useRef, useState } from 'react';
-import ColorPicker from './ColorPicker';
+interface MyBoard {
+    brushColor: string;
+    brushSize: number;
+}
 
-const Board = () => {
+const Board: React.FC<MyBoard> = (props) => {
 
-    const canvasRef = useRef<HTMLCanvasElement>(null);
-    const [color, setColor] = useState('#000000'); 
+    const { brushColor, brushSize } = props;
+    const canvasRef = useRef<HTMLCanvasElement | null>(null);
+
+    const [socket, setSocket] = useState<Socket | null>(null);
+
+    useEffect(() => {
+        const newSocket = io('http://localhost:5000');
+        console.log(newSocket, "Connected to socket");
+        setSocket(newSocket);
+    }, []);
+
 
     useEffect(() => {
 
+        if (socket) {
+            // Event listener for receiving canvas data from the socket
+            socket.on('canvasImage', (data) => {
+                // Create an image object from the data URL
+                const image = new Image();
+                image.src = data;
+
+                const canvas = canvasRef.current;
+                if (canvas) {
+                  const ctx = canvas.getContext('2d');
+                  if (ctx) {
+                    // Draw the image onto the canvas
+                    image.onload = () => {
+                      ctx.drawImage(image, 0, 0);
+                    };
+                  }
+                }
+            });
+        }
+    }, [socket]);
+
+
+    // Function to start drawing
+    useEffect(() => {
 
         // Variables to store drawing state
         let isDrawing = false;
         let lastX = 0;
         let lastY = 0;
-
-
         const startDrawing = (e: { offsetX: number; offsetY: number; }) => {
             isDrawing = true;
 
-
+            console.log(`drawing started`, brushColor, brushSize);
             [lastX, lastY] = [e.offsetX, e.offsetY];
         };
-
 
         // Function to draw
         const draw = (e: { offsetX: number; offsetY: number; }) => {
             if (!isDrawing) return;
-
 
             const canvas = canvasRef.current;
             const ctx = canvas && canvas.getContext('2d');
@@ -39,68 +71,83 @@ const Board = () => {
                 ctx.stroke();
             }
 
-
             [lastX, lastY] = [e.offsetX, e.offsetY];
         };
 
-
         // Function to end drawing
         const endDrawing = () => {
+            const canvas = canvasRef.current;
+            const dataURL = canvas && canvas.toDataURL(); // Get the data URL of the canvas content
+
+            // Send the dataURL or image data to the socket
+            // console.log('drawing ended')
+            if (socket) {
+                socket.emit('canvasImage', dataURL);
+                console.log('drawing ended')
+            }
             isDrawing = false;
         };
-
 
         const canvas: HTMLCanvasElement | null = canvasRef.current;
         const ctx = canvasRef.current?.getContext('2d');
 
-
         // Set initial drawing styles
         if (ctx) {
-            ctx.strokeStyle = color;
-            ctx.lineWidth = 5;
-
+            ctx.strokeStyle = brushColor;
+            ctx.lineWidth = brushSize;
 
             ctx.lineCap = 'round';
             ctx.lineJoin = 'round';
 
-
         }
         // Event listeners for drawing
         if(canvas){
-
+            console.log("Canvas1 Triggered")
             canvas.addEventListener('mousedown', startDrawing);
             canvas.addEventListener('mousemove', draw);
             canvas.addEventListener('mouseup', endDrawing);
             canvas.addEventListener('mouseout', endDrawing);
         }
 
-
         return () => {
             // Clean up event listeners when component unmounts
             if(canvas){
-
+                console.log("Canvas2 Triggered::")
                 canvas.removeEventListener('mousedown', startDrawing);
                 canvas.removeEventListener('mousemove', draw);
                 canvas.removeEventListener('mouseup', endDrawing);
                 canvas.removeEventListener('mouseout', endDrawing);
             }
         };
+    }, [brushColor, brushSize, socket]);
+
+
+    const [windowSize, setWindowSize] = useState([
+        window.innerWidth,
+        window.innerHeight,
+    ]);
+
+    useEffect(() => {
+        const handleWindowResize = () => {
+            setWindowSize([window.innerWidth, window.innerHeight]);
+        };
+
+        window.addEventListener('resize', handleWindowResize);
+
+        return () => {
+            window.removeEventListener('resize', handleWindowResize);
+        };
     }, []);
 
 
     return (
-        <>
-        <ColorPicker onChange={setColor}/>
         <canvas
             ref={canvasRef}
-            width={600}
-            height={400}
-            // TODO:: Change color to white
-            style={{ backgroundColor: 'red' }}
-            />
-            </>
+            width={windowSize[0] > 600 ? 600 : 300}
+            height={windowSize[1] > 400 ? 400 : 200}
+            style={{ backgroundColor: 'white' }}
+        />
     );
 };
-
 
 export default Board;
