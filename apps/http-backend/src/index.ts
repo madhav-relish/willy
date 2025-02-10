b98e38bf14d095387dda6fcd733e4f61e702eec2
Madhav <madhavsingh.relish@gmail.com>
2025-02-11 00:04:14 +0530

    Added a signin route logic

--- apps/http-backend/src/index.ts
+++ apps/http-backend/src/index.ts
@@ -21,8 +21,22 @@ app.post('/signup', (req, res) => {
     return res.json({ token: token, message: "User created Successfully!" })
 })
 
+//@ts-ignore
 app.post('/signin', (req, res) => {
 
+    const userInfo = req.body
+    if (!userInfo) {
+        console.log("No user data recieved in body")
+        return res.json({ message: "No data was sent in body" })
+    }
+    // Search for the user in DB and get the user ID
+    const userId = "123123"
+    // Sign the token
+    const token = jwt.sign(userId, JWT_SECRET)
+    // Return the token and appropriate info
+    res.json({
+        token
+    })
 })
 
 app.post('/room', (req, res) => {
