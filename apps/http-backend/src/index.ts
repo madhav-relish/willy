7ffd23fe8bb66c56c771041b26312c27b235255e
Madhav <madhavsingh.relish@gmail.com>
2025-02-10 23:56:00 +0530

    Added a basic signup route with basic logic

--- apps/http-backend/src/index.ts
+++ apps/http-backend/src/index.ts
@@ -2,17 +2,30 @@ import express from "express"
 import jwt from "jsonwebtoken"
 
 const app = express()
+const JWT_SECRET = "helloworld"
 
 // Add zod validation
-app.post('/signup', (req, res)=>{
+//@ts-ignore
+app.post('/signup', (req, res) => {
+    const userInfo = req.body
+    if (!userInfo) {
+        console.log("No user data recieved in body")
+        return res.json({ message: "No data was sent in body" })
+    }
+    //Create the user
+    const userId = "123123"
+    // Sign the jwt
+    const token = jwt.sign(userId, JWT_SECRET)
+
+    // Return the jwt to the user
+    return res.json({ token: token, message: "User created Successfully!" })
+})
+
+app.post('/signin', (req, res) => {
 
 })
 
-app.post('/signin', (req, res)=>{
-
-})
-
-app.post('/room', (req, res)=>{
+app.post('/room', (req, res) => {
 
 })
 
