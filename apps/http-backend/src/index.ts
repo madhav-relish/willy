f3f0a1cae285394a84fa81faf1f034a9e4b7e9d8
Madhav <madhavsingh.relish@gmail.com>
2025-02-22 23:45:04 +0530

    Signup route

--- apps/http-backend/src/index.ts
+++ apps/http-backend/src/index.ts
@@ -2,51 +2,59 @@ import { JWT_SECRET } from "@repo/backend-common/config"
 import { prismaClient } from "@repo/db/client"
 import express from "express"
 import jwt from "jsonwebtoken"
+import {CreateUserSchema} from '@repo/common/types'
 
 const app = express()
+app.use(express.json())
 
 // Add zod validation
 //@ts-ignore
-app.post('/signup', (req, res) => {
-    const userInfo = req.body
-    if (!userInfo) {
-        console.log("No user data recieved in body")
-        return res.json({ message: "No data was sent in body" })
+app.post('/signup', async(req, res) => {
+    const parsedData = CreateUserSchema.safeParse(req.body);
+    if (!parsedData.success) {
+        console.log(parsedData.error);
+        res.json({
+            message: "Incorrect inputs"
+        })
+        return;
     }
-    //Create the user
-    // const userId = "123123"
-    const userId = prismaClient.user.create({
-        data:{
-            email: userInfo.email,
-            password: userInfo.password,
-            name: userInfo.name
-        }
-    })
-    // Sign the jwt
-    const token = jwt.sign(userId, JWT_SECRET)
-
-    // Return the jwt to the user
-    return res.json({ token: token, message: "User created Successfully!" })
+    try {
+        const user = await prismaClient.user.create({
+            data: {
+                email: parsedData.data?.username,
+                // TODO: Hash the pw
+                password: parsedData.data.password,
+                name: parsedData.data.name,
+            }
+        })
+        res.json({
+            userId: user.id
+        })
+    } catch(e) {
+        res.status(411).json({
+            message: "User already exists with this username"
+        })
+    }
 })
 
 //@ts-ignore
-app.post('/signin', (req, res) => {
+app.post('/signin', async(req, res) => {
 
-    const userInfo = req.body
+    const userInfo = signInSchemareq.body
     if (!userInfo) {
         console.log("No user data recieved in body")
         return res.json({ message: "No data was sent in body" })
     }
     // Search for the user in DB and get the user ID
     // const userId = "123123"
-    const userId = prismaClient.user.findUnique({
+    const user = await prismaClient.user.findUnique({
         where: {
             email: userInfo.email,
             password: userInfo.password
         }
     })
     // Sign the token
-    const token = jwt.sign(userId, JWT_SECRET)
+    const token = jwt.sign(user?.id, JWT_SECRET)
     // Return the token and appropriate info
     res.json({
         token
@@ -54,12 +62,12 @@ app.post('/signin', (req, res) => {
 })
 
 //create room
-app.post('/create-room', (req, res, middleware) => {
+app.post('/create-room', async(req, res, middleware) => {
 
 // create the room
 // Return roomID
 
-    const roomId = prismaClient.room.create({
+    const roomId = await prismaClient.room.create({
         data: {
              slug: req
         }
