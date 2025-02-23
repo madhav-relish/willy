00aeffcd71b175dce93021e80597e96a28a1b864
Madhav <madhavsingh.relish@gmail.com>
2025-02-23 11:26:22 +0530

    Fixed the middleware token extraction

--- apps/http-backend/src/middleware.ts
+++ apps/http-backend/src/middleware.ts
@@ -4,11 +4,14 @@ import jwt from "jsonwebtoken";
 
 export function middleware (req: Request, res: Response, next:NextFunction){
 
-    const token = req.headers["authorization"] ?? ""
+    const BearerToken = req.headers["authorization"] ?? ""
+    const token = BearerToken.split(" ")[1]
+
     if(!token){
         res.status(403).json({
             error: "No authorization token was provided"
         })
+        return;
     }
     const decoded = jwt.verify(token, JWT_SECRET)
 
