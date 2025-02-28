01705add588e34e6a7f300165c487c748b2f2f89
Madhav <madhavsingh.relish@gmail.com>
2025-02-28 15:13:00 +0530

    Zod schema update

--- packages/common/src/index.ts
+++ packages/common/src/index.ts
@@ -1,15 +1,15 @@
 import { z } from "zod";
 
 export const CreateUserSchema = z.object({
-    username: z.string().min(3).max(40),
-    password: z.string(),
-    name: z.string(),
+    username: z.string().email(),
+    password: z.string().min(8, {message: "Password should be minimum 8 characters long"}),
+    name: z.string().min(2, {message: "Name should be minimum 2 characters long"}),
     avatar: z.string().optional()
 })
 
 export const SigninSchema = z.object({
-    username: z.string().min(3).max(40),
-    password: z.string(),
+    username: z.string().email(),
+    password: z.string().min(8, {message: "Password should be minimum 8 characters long"}),
 })
 
 export const CreateRoomSchema = z.object({
