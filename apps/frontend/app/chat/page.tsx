399ea168c2c78132485c1245eb06d3900d6d854b
Madhav <madhavsingh.relish@gmail.com>
2025-02-28 18:06:17 +0530

    Create room api integration

--- apps/frontend/app/chat/page.tsx
+++ apps/frontend/app/chat/page.tsx
@@ -1,11 +1,41 @@
-import React from 'react'
+"use client";
 
-type Props = {}
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import axios from "axios";
+import { useRouter } from "next/navigation";
+import React, { useState } from "react";
+import { toast } from "sonner";
 
-const Chat = (props: Props) => {
+const Chat = () => {
+  const [roomName, setRoomName] = useState("");
+  const router = useRouter()
+  const handleCreateRoom = async()=>{
+    try{
+        const response = await axios.post('http://localhost:3002/create-room',{
+            name: roomName
+        })
+        toast.success('Room created successfully!')
+        router.push(`/chat?${response.data?.room?.id}`)
+    }catch(error){
+        console.error("Error while creating the room", error)
+        toast.error("Error while creating the room")
+    }
+  }
   return (
-    <div>Chat</div>
-  )
-}
+    <div>
+      <Input
+        value={roomName}
+        onChange={(e) => {
+          console.log("Entered values::", e.target.value);
+          setRoomName(e.target.value);
+        }}
+        placeholder="Enter a room name"
+      />
+      <Button onClick={handleCreateRoom}>Create Room</Button>
+      <Button>Join Room</Button>
+    </div>
+  );
+};
 
-export default Chat
+export default Chat;
