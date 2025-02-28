a57cf8a17c48cd8da0103b8f0481be1ed099b556
Madhav <madhavsingh.relish@gmail.com>
2025-02-28 13:39:13 +0530

    Added auth routes

---
+++ apps/frontend/app/(auth)/signin/page.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+type Props = {}
+
+const Signin = (props: Props) => {
+  return (
+    <div>
+        Hello from signin page
+    </div>
+  )
+}
+
+export default Signin
---
+++ apps/frontend/app/(auth)/signup/page.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+type Props = {}
+
+const Signup = (props: Props) => {
+  return (
+    <div>
+        Hello from signup page
+    </div>
+  )
+}
+
+export default Signup
