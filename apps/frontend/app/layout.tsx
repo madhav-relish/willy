1e90295286678e685e4f9f1d63a94d36ff037f31
Madhav <madhavsingh.relish@gmail.com>
2025-02-28 17:50:08 +0530

    Added toasts

--- apps/frontend/app/layout.tsx
+++ apps/frontend/app/layout.tsx
@@ -3,6 +3,7 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { Suspense } from "react";
 import { ThemeProvider } from "@/components/ui/ThemeProvider";
+import { Toaster } from "@/components/ui/sonner";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -36,6 +37,7 @@ export default function RootLayout({
             disableTransitionOnChange
           >
             {children}
+            <Toaster/>
           </ThemeProvider>
         </Suspense>
       </body>
