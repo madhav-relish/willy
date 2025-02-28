e18673ca9f8f5503919e386b70a64f172b6dc0e1
Madhav <madhavsingh.relish@gmail.com>
2025-02-28 15:05:15 +0530

    Added form errors

--- apps/frontend/app/(auth)/signin/page.tsx
+++ apps/frontend/app/(auth)/signin/page.tsx
@@ -1,4 +1,4 @@
-'use client'
+"use client";
 import { Button } from "@/components/ui/button";
 import {
   Card,
@@ -12,37 +12,49 @@ import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
 import { SigninSchema } from "@repo/common/types";
 import z from "zod";
-import {useForm} from 'react-hook-form'
-import {zodResolver} from '@hookform/resolvers/zod'
-
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
 
 type Props = {};
 
-type signinFromValues = z.infer<typeof SigninSchema>
+type signinFromValues = z.infer<typeof SigninSchema>;
 
 const Signin = (props: Props) => {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<signinFromValues>({
+    resolver: zodResolver(SigninSchema),
+  });
 
-  const {register, handleSubmit, formState:{errors}} = useForm<signinFromValues>({
-    resolver: zodResolver(SigninSchema)
-  })
-
-  const onSubmit = (data: signinFromValues)=>{
-    console.log("Submitted data::", data)
-  }
+  const onSubmit = (data: signinFromValues) => {
+    console.log("Submitted data::", data);
+  };
 
   return (
     <>
-      <form onSubmit={handleSubmit(onSubmit)} className="flex h-screen justify-center items-center p-2">
+      <form
+        onSubmit={handleSubmit(onSubmit)}
+        className="flex h-screen justify-center items-center p-2"
+      >
         <Card className="w-[450px] min-w-[300px]">
           <CardHeader>
             <CardTitle>Signin</CardTitle>
           </CardHeader>
           <CardContent>
-            <form>
+            <div>
               <div className="grid w-full items-center gap-4">
                 <div className="flex flex-col space-y-1.5">
                   <Label htmlFor="email">Username</Label>
-                  <Input id="email" placeholder="example@gmail.com" type="email" required {...register("username")}/>
+                  <Input
+                    id="email"
+                    placeholder="example@gmail.com"
+                    type="email"
+                    required
+                    {...register("username")}
+                  />
+                   {errors.username && <p className="text-red-500 text-sm">{errors.username.message}</p>}
                 </div>
                 <div className="flex flex-col space-y-1.5">
                   <Label htmlFor="password">Password</Label>
@@ -53,9 +65,10 @@ const Signin = (props: Props) => {
                     required
                     {...register("password")}
                   />
+                   {errors.password && <p className="text-red-500 text-sm">{errors.password.message}</p>}
                 </div>
               </div>
-            </form>
+            </div>
           </CardContent>
           <CardFooter className="flex justify-between">
             <Button type="submit">Sign in</Button>
