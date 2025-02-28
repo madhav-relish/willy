339ac361e51f2839f4c99962b57bb1a8703977a7
Madhav <madhavsingh.relish@gmail.com>
2025-02-28 13:54:05 +0530

    Created signup form layout

--- apps/frontend/app/(auth)/signup/page.tsx
+++ apps/frontend/app/(auth)/signup/page.tsx
@@ -1,11 +1,54 @@
-import React from 'react'
+import { Button } from "@/components/ui/button"
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card"
+import { Input } from "@/components/ui/input"
+import { Label } from "@/components/ui/label"
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select"
 
 type Props = {}
 
 const Signup = (props: Props) => {
   return (
-    <div>
-        Hello from signup page
+    <div className="flex h-screen justify-center items-center p-2">
+         <Card className="w-[450px] min-w-[300px]">
+      <CardHeader>
+        <CardTitle>Signup Now!</CardTitle>
+        <CardDescription>Get started by creating an account</CardDescription>
+      </CardHeader>
+      <CardContent>
+        <form>
+          <div className="grid w-full items-center gap-4">
+            <div className="flex flex-col space-y-1.5">
+              <Label htmlFor="name">Name</Label>
+              <Input id="name" placeholder="John Doe" />
+            </div>
+            <div className="flex flex-col space-y-1.5">
+            <Label htmlFor="email">Email</Label>
+            <Input id="email" placeholder="example@gmail.com" />
+            </div>
+            <div className="flex flex-col space-y-1.5">
+            <Label htmlFor="email">Password</Label>
+            <Input type="password" id="password" placeholder="*********" />
+            </div>
+          </div>
+        </form>
+      </CardContent>
+      <CardFooter className="flex justify-between">
+        <Button>Sign up</Button>
+      </CardFooter>
+    </Card>
     </div>
   )
 }
