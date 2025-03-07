b46d5f73b14a3cbc56cd22e010f4f2010a454995
Madhav <madhavsingh.relish@gmail.com>
2025-03-07 16:18:39 +0530

    UI Components

--- apps/frontend/components/Topbar.tsx
+++ apps/frontend/components/Topbar.tsx
@@ -1,23 +1,21 @@
-'use client'
-import React, { ReactNode } from 'react'
-import { SidebarTrigger } from './ui/sidebar'
-import { useTopbar } from '@/store/useTopbar'
-
+"use client";
+import React, { ReactNode } from "react";
+import { SidebarTrigger } from "./ui/sidebar";
+import { useTopbar } from "@/store/useTopbar";
 
 const Topbar = () => {
-  const {title, component} = useTopbar()
+  const { title, component } = useTopbar();
   return (
     <div className="fixed top-0 flex items-center justify-between bg-background z-50 h-12 w-3/4 p-3 border-b mb-4">
-    <SidebarTrigger />
-    <div>
-      <div className='flex items-center gap-8'>
-
-   <div>{title}</div>
-   <div>{component}</div>
+      <SidebarTrigger />
+      <div>
+        <div className="flex items-center gap-8">
+          <div>{title}</div>
+          <div>{component}</div>
+        </div>
       </div>
     </div>
-  </div>
-  )
-}
+  );
+};
 
-export default Topbar
+export default Topbar;
